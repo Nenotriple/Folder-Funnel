f4e6dc06fe549901799c1133d7c7e52522491b9a
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2025-02-26 16:56:59 -0800

    Update duplicate_handler.py

--- duplicate_handler.py
+++ duplicate_handler.py
@@ -13,7 +13,7 @@ from difflib import SequenceMatcher
 #region - File Operations
 
 
-def are_files_identical(file1, file2, rigorous_check=False, method='Strict', max_files=10, chunk_size=8192):
+def are_files_identical(file1, file2, check_mode="By Size", method='Strict', max_files=10, chunk_size=8192):
     """Compare files by size/MD5 or find similar files if rigorous_check is True."""
     def get_md5(filename):
         m = hashlib.md5()
@@ -30,12 +30,12 @@ def are_files_identical(file1, file2, rigorous_check=False, method='Strict', max
         for file in similar_files:
             if os.path.exists(file) and os.path.getsize(file1) == os.path.getsize(file):
                 return True
-        if rigorous_check:
+        if check_mode == "By MD5":
             file1_md5 = get_md5(file1)
             for file in similar_files:
                 if get_md5(file) == file1_md5:
                     return True
-        else:
+        elif check_mode == "By Size":
             if os.path.exists(file2) and get_md5(file1) == get_md5(file2):
                 return True
         return False
